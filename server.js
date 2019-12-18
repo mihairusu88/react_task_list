38146430f7346bc2638c995e24e785e6e7a371ef
Mihai Rusu <mihai.rusu88@yahoo.com>
2019-12-18 18:11:32 +0200

    Fixed some code for heroku build

--- server.js
+++ server.js
@@ -17,9 +17,19 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use('/api/tasks', tasksRouter);
 
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 const host = '0.0.0.0';
 const port = process.env.PORT || 5000;
 
 app.listen(port, host, function () {
-    console.log(`Server is running on port ${process.env.PORT}`);
+    console.log(`Server is running on port ${port}`);
 });
